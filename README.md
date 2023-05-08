16b20e7d9c935af9d0efc0521d807a892d325f32
ktdthoainam <129750448+ktdthoainam@users.noreply.github.com>
2023-05-08 21:33:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js_mang
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ubanhw)
---
+++ script.js
@@ -0,0 +1,33 @@
+// console.log('Hello!');
+// let fruits =["coc","me","xoai","oi"]
+// console.log(fruits)
+// fruits.push("cam");//thêm vào vị trí cuối//nên dùng
+// console.log(fruits)
+// fruits[2]="man";//thêm vào vị trí 2+1
+// fruits[fruits.length]="tao";//thêm vào vị trí cuối
+// console.log(fruits.length, fruits)
+let numbers=[1,2,3,4,5,6,9,7];
+// console.log(numbers);
+// console.log(numbers.join("+"));
+
+// console.log(numbers.reverse())
+// console.log(numbers.sort())
+// numbers.unshift("nam");//them phan tuw dau mang
+// console.log(numbers);
+// numbers.shift()//xoa phan tu dau mang
+// console.log(numbers);
+// numbers.pop();//xoa phan tu cuoi cung
+// console.log(numbers);
+// delete numbers[5];
+// console.log(numbers);
+// for (let i = 0, i<=numbers.length,i++)
+// {
+//   console.log(numbers[3])
+
+// }
+for(let i in numbers)
+{
+  console.log(numbers[i])
+}
+
+
